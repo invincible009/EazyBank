0a8096a291125a074b5b2bf0c1b060b87c12d9e5
Gabriel Simon Mbaiorga <msgtoolz@gmail.com>
2023-01-26 14:35:40 +0100

    add public and protected base api controllers to reduce the code reduancy in the spring security

---
+++ src/main/java/com/sdl/eazybank/controllers/ProtectedBaseApiController.java
@@ -0,0 +1,11 @@
+package com.sdl.eazybank.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RequestMapping("/api/")
+public class ProtectedBaseApiController {
+
+  public ProtectedBaseApiController() {
+  }
+}
