e83028952176ff6c7be1112a794020ae31210821
Gabriel Simon Mbaiorga <msgtoolz@gmail.com>
2023-01-27 17:45:29 +0100

    customize cors security configuration

--- src/main/java/com/sdl/eazybank/config/ProjectSecurityConfig.java
+++ src/main/java/com/sdl/eazybank/config/ProjectSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.sdl.eazybank.config;
 
+import java.util.Collections;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -7,6 +9,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 public class ProjectSecurityConfig {
@@ -15,7 +19,15 @@ public class ProjectSecurityConfig {
   public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity httpSecurity)
       throws Exception {
     httpSecurity
-        .csrf().disable()
+        .cors().configurationSource(request -> {
+          CorsConfiguration configuration = new CorsConfiguration();
+          configuration.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+          configuration.setAllowedMethods(Collections.singletonList("*"));
+          configuration.setAllowCredentials(true);
+          configuration.setAllowedHeaders(Collections.singletonList("*"));
+          configuration.setMaxAge(3600L);
+          return configuration;
+        }).and().csrf().disable()
         .authorizeHttpRequests((auth) ->
             auth.antMatchers("/app/v1/").authenticated()
                 .antMatchers("/api/")
