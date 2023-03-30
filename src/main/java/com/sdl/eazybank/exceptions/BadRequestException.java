5ee5837de3d59a9f6a492142d4d05066c5d49513
mbaiorga <gabrielmbaiorga@gmail.com>
2023-03-30 22:18:43 +0100

    add serial annotation to the serial number. Indicating that it is part of a serial mechanism.

--- src/main/java/com/sdl/eazybank/exceptions/BadRequestException.java
+++ src/main/java/com/sdl/eazybank/exceptions/BadRequestException.java
@@ -3,8 +3,11 @@ package com.sdl.eazybank.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.io.Serial;
+
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class BadRequestException  extends RuntimeException{
+    @Serial
     private static final long serialVersionUID = 6171978443681353425L;
 
     public BadRequestException(String message) {
--- src/main/java/com/sdl/eazybank/exceptions/ConflictRequestException.java
+++ src/main/java/com/sdl/eazybank/exceptions/ConflictRequestException.java
@@ -3,8 +3,11 @@ package com.sdl.eazybank.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.io.Serial;
+
 @ResponseStatus(HttpStatus.CONFLICT)
 public class ConflictRequestException extends RuntimeException{
+    @Serial
     private static final long serialVersionUID = 6171978443681353425L;
 
     public ConflictRequestException(String message) {
